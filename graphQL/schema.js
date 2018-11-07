8f9d7fad749e3ee46a21d11c6e99999f977cc181
Andika Nugraha <m.andika.nugraha@gmail.com>
2018-11-07 13:42:54 +0700

    Issue closed

--- graphQL/schema.js
+++ graphQL/schema.js
@@ -2,6 +2,12 @@
 // const { buildSchema } = require('graphql');
 const { makeExecutableSchema } = require('graphql-tools')
 const resolvers = require('./resolver');
+const mongoose = require('mongoose');
+
+const { ObjectId } = mongoose.Types;
+ObjectId.prototype.valueOf = function () {
+  return this.toString()
+}
 
 const typeDefs = 
 `type Product {
